711e5742e92b637d3fe51b42fb867a5264579211
Aleksandr Podcherednik <startcpa1@yandex.ru>
2024-05-14 00:49:05 +0300

    add func clear debt

--- networks/admin.py
+++ networks/admin.py
@@ -2,7 +2,28 @@ from django.contrib import admin
 
 from networks.models import Factory, RetailNetwork, IndividualEntrepreneur
 
-admin.site.register(Factory)
-admin.site.register(RetailNetwork)
-admin.site.register(IndividualEntrepreneur)
 
+class NetworkNodeAdmin(admin.ModelAdmin):
+    list_filter = ('city',)
+
+    def clear_the_debt(self, queryset):
+        queryset.update(debt=0)
+
+    clear_the_debt.short_description = "Очистить задолженность"
+
+    actions = [clear_the_debt]
+
+
+@admin.register(Factory)
+class FactoryAdmin(NetworkNodeAdmin):
+    list_display = ('name', 'country', 'city', 'debt',)
+
+
+@admin.register(RetailNetwork)
+class RetailNetworkAdmin(NetworkNodeAdmin):
+    list_display = ('name', 'country', 'city', 'debt',)
+
+
+@admin.register(IndividualEntrepreneur)
+class IndividualEntrepreneurAdmin(NetworkNodeAdmin):
+    list_display = ('name', 'country', 'city', 'debt',)
